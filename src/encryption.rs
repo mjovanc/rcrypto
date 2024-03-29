b4eb6aca0f80e1071edd95e1ffb4d913b9fd3d2d
mjovanc <mjovanc@icloud.com>
2024-03-29 10:35:03 +0100

    Finishing functions

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,14 +1,15 @@
-use aes_gcm::{aead::{Aead, AeadCore, KeyInit, OsRng}, Aes256Gcm, AesGcm, Key};
+use aes_gcm::{aead::{Aead, KeyInit}, Aes256Gcm, AesGcm, Key};
 use aes_gcm::aead::consts::U12;
 use aes_gcm::aead::Nonce;
 use aes_gcm::aes::Aes256;
 
 pub fn encrypt_aes256(
     key: &Key<AesGcm<Aes256, U12>>,
-    nonce: &Nonce<AesGcm<Aes256, U12>>
+    nonce: &Nonce<AesGcm<Aes256, U12>>,
+    text: &[u8],
 ) -> Result<Vec<u8>, aes_gcm::Error> {
     let cipher = Aes256Gcm::new(&key);
-    let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref())?;
+    let ciphertext = cipher.encrypt(&nonce, text.as_ref())?;
 
     Ok(ciphertext)
 }
@@ -16,10 +17,10 @@ pub fn encrypt_aes256(
 pub fn decrypt_aes256(
     key: &Key<AesGcm<Aes256, U12>>,
     nonce: &Nonce<AesGcm<Aes256, U12>>,
-    cipher_text: Vec<u8>
+    cipher: Vec<u8>
 ) -> Result<Vec<u8>, aes_gcm::Error>  {
-    let cipher = Aes256Gcm::new(&key);
-    let plaintext = cipher.decrypt(&nonce, cipher_text.as_ref())?;
+    let cipher_inst = Aes256Gcm::new(&key);
+    let plaintext = cipher_inst.decrypt(&nonce, cipher.as_ref())?;
 
     Ok(plaintext)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::string::FromUtf8Error;
 use aes_gcm::aead::OsRng;
-use aes_gcm::{AeadCore, Aes256Gcm, Error, KeyInit};
+use aes_gcm::{AeadCore, Aes256Gcm, KeyInit};
 use crate::encryption::{decrypt_aes256, encrypt_aes256};
 
 mod encryption;
@@ -8,9 +7,12 @@ mod encryption;
 fn main() {
     // Generate a random encryption key
     let key = Aes256Gcm::generate_key(OsRng);
+    // Generate a nonce value
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let text = b"Some text to be encrypted...".as_ref();
 
-    let cipher = match encrypt_aes256(&key, &nonce) {
+    // Encrypt
+    let cipher = match encrypt_aes256(&key, &nonce, &text) {
         Ok(cipher_text) => cipher_text,
         Err(err) => {
             eprintln!("Error encrypting using AES-256, {}", err);
@@ -20,6 +22,7 @@ fn main() {
 
     println!("Cipher text: {:?}", cipher);
 
+    // Decrypt cipher text
     let plaintext_bytes = match decrypt_aes256(&key, &nonce, cipher) {
         Ok(plaintext) => plaintext,
         Err(err) => {
@@ -31,7 +34,7 @@ fn main() {
     println!("Plaintext (bytes): {:?}", plaintext_bytes);
 
     match String::from_utf8(plaintext_bytes) {
-        Ok(str) => println!("Plaintext: {:?}", str),
+        Ok(str) => println!("Plaintext (String): {:?}", str),
         Err(err) => {
             eprintln!("Error converting bytes array to String, {}", err);
             return;
