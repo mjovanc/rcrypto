f4404b58025a40ae821d7dcc157535d285ef6720
mjovanc <mjovanc@icloud.com>
2024-03-29 11:00:23 +0100

    Adding unit test

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,7 +1,8 @@
-use aes_gcm::{aead::{Aead, KeyInit}, Aes256Gcm, AesGcm, Key};
+use aes_gcm::{aead::{Aead, KeyInit}, AeadCore, Aes256Gcm, AesGcm, Key};
 use aes_gcm::aead::consts::U12;
-use aes_gcm::aead::Nonce;
+use aes_gcm::aead::{Nonce, OsRng};
 use aes_gcm::aes::Aes256;
+use std::str;
 
 pub fn encrypt_aes256(
     key: &Key<AesGcm<Aes256, U12>>,
@@ -25,3 +26,20 @@ pub fn decrypt_aes256(
     Ok(plaintext)
 }
 
+#[test]
+fn encrypt_and_decrypt_test() {
+    let key = Aes256Gcm::generate_key(OsRng);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let text = b"Some text to be encrypted...".as_ref();
+
+    // Encrypt text
+    let cipher = encrypt_aes256(&key, &nonce, &text).unwrap();
+
+    // Decrypt text to bytes vector
+    let plaintext_bytes = decrypt_aes256(&key, &nonce, cipher).unwrap();
+
+    let text_str = str::from_utf8(&text).unwrap();
+    let initial_str = String::from(text_str);
+    let result_str = String::from_utf8(plaintext_bytes).unwrap();
+    assert_eq!(initial_str, result_str);
+}
